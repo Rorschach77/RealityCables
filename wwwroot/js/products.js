7969d82cf360c42887ef1d75a4fb5237450b5c81
Sean Bytnar <sdbytnar@gmail.com>
2020-09-16 06:08:11 -0500

    Initial commit

---
+++ wwwroot/js/products.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+	$.getJSON("/data.json", function (data) {
+		var items = [];
+		$.each(data.products, function (index, product) {
+			items.push("<div class='row'>"
+				+ "<div class='col-sm-3 center'>" + "<img src='" + product.image + "' width='250' />" + "</div>"
+				+ "<div class='col-sm-9'>" + product.name + "<br/>" + product.description + "<br/>" + getPriceDisplay(product) + "</div>"
+				+ "</div>");
+		});
+
+		$("<div />", {
+			html: items.join("")
+		}).appendTo("main");
+	});
+});
+
+function getPriceDisplay(product) {
+	return "$" + product.basePrice + " for a " + product.baseLength + " feet<br/>";
+}
